db61989b899ce74a411da33e7d26918e432f20ac
Joshua <olajidejoshua4real@gmail.com>
2022-10-16 15:35:51 +0100

    Feat: Add Get tour functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,4 @@
 const Tour = require('./../models/tourModel');
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-/**
- * A middleware often takes in 4 params which are
- * @param {req} req -> Request
- * @param {res} res -> Response
- * @param {next} next -> Next that always continues running the function once it's done
- * @param {val} val -> Value
- */
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    // Add return inorder to avoid it setting the header twice
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Id Not found'
-    });
-  }
-  next();
-};
 
 exports.checkBody = (req, res, next) => {
   if (req.body.name || req.body.price) {
@@ -33,46 +10,43 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    result: tours.length,
-    data: {
-      tours
-    }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      // requestedAt: req.requestTime,
+      // result: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1; // (A nice way to convert string to integer)
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/../dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   err => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour
-  //       }
-  //     });
-  //     return err;
-  //   }
-  // );
-
   try {
     const newTour = await Tour.create(req.body);
     res.status(200).json({
