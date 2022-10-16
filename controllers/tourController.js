a8cf94a9f58a1c010a4471821554353d636c46dd
Joshua <olajidejoshua4real@gmail.com>
2022-10-16 18:08:10 +0100

    Feat: Create auto import development data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -84,9 +84,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'Tour successfully deleted',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
