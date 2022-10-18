1583db2024b0101d759597eaa34223d6952bbd7a
Joshua <olajidejoshua4real@gmail.com>
2022-10-18 21:36:41 +0100

    Feat: Add MonthlyPlan method to calculate monthly plan

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 // Define routes
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/top-5-cheap')
--- server.js
+++ server.js
@@ -24,3 +24,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+// MONGODB PLAYGROUND - https://mongoplayground.net/p/qr0RiU7CgWX
