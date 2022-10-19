625abdc52fe70f96d53644d3d727a5243fc6b17c
Joshua <olajidejoshua4real@gmail.com>
2022-10-19 14:02:16 +0100

    Feat: Add presave document middleware to slugify name property

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -90,6 +91,12 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
